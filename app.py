b9d000de189202fd267d96d82424816b36113f47
freskey <frowkzey@gmail.com>
2024-06-21 10:14:30 +0200

    rewrite of the index.js + implementation of modif button

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
 from flask import Flask, render_template, request
 from db import get_from_db, load_to_db
+import json
 
 app = Flask(__name__)
 
-tasks = get_from_db()
+tasks = []
 
 @app.route("/")
 def home():
-    return render_template("index.html", tasks=tasks)
+    return render_template("index.html")
 
 @app.route("/index.js")
 def script():
     return render_template("index.js")
 
-@app.route("/api", methods=["POST"])
+@app.route("/api", methods=["POST", "GET"])
 def api():
     global tasks
-    if request.method == "POST":
-        tasks = request.json
-        load_to_db(tasks)
-    return "done"
+    match request.method:
+        case "POST":
+            tasks = []
+            load_to_db(tasks)
+            return "done"
+        case "GET":
+            tasks = get_from_db()
+            return json.dumps(tasks)
 
 app.run(debug=True)
 
--- templates/index.js
+++ templates/index.js
@@ -1,142 +1,132 @@
 const taskContainerDiv = document.getElementById("task-container")
-// apply the interaction to the already existing tasks
-for (element of taskContainerDiv.children) {
-    const checkbox = element.children[0]
-    const label = element.children[1]
-    const cancelButton = element.children[2]
-    const deleteButton = element.children[3]
-    deleteButton.addEventListener("click", () => {
-        deleteButton.parentNode.remove()
-    })
-    cancelButton.addEventListener("click", () => {
-        if (checkbox.checked) 
-            return
-        if (label.value == "") {
-            alert("Can't cancel an empty task")
-            return
+
+// -- REQUESTS --
+let xmlHttp = new XMLHttpRequest();
+
+const getTasks = () => {
+    xmlHttp.open("GET", "/api", false);
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.responseText);
+}
+
+const sendTasks = (tasks) => {
+    xmlHttp.open("POST", "/api", false);
+    xmlHttp.send(JSON.stringify(tasks));
+    return xmlHttp.responseText;
+}
+
+// -- TASKS --
+const createTask = (task) => {
+    // add the modif state
+    task.push(false);
+
+    const taskDiv = document.createElement('div');
+
+    const checkbox = document.createElement('input');
+    const label = document.createElement('input');
+    const cancelButton = document.createElement('input');
+    const deleteButton = document.createElement('input');
+    const modifButton = document.createElement('input');
+
+    taskDiv.classList.add('task');
+
+    checkbox.type = 'checkbox';
+    checkbox.checked = task[1];
+
+    label.type = 'text';
+    label.value = task[0];
+    label.placeholder = 'remplir';
+    label.disabled = task[2];
+    label.readOnly = task[3];
+
+    cancelButton.type = 'button';
+    cancelButton.value = task[2] ? 'enable' : 'disable';
+    cancelButton.classList.add('cancel-button');
+
+    deleteButton.type = 'button';
+    deleteButton.value = 'delete';
+    deleteButton.classList.add('delete-button');
+
+    modifButton.type = 'button';
+    modifButton.value = 'save';
+
+    taskDiv.append(checkbox, label, modifButton, cancelButton, deleteButton);
+    taskContainerDiv.appendChild(taskDiv);
+
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked)
+        {
+            if (label.value=='')
+            {
+                checkbox.checked = false;
+                alert("Can't check an empty task");
+            }
+            else
+            {
+                label.disabled = true;
+                cancelButton.disabled = true;
+            }
         }
-        // freeze all taks's inputs except this one
-        const value = label.disabled ? false : true
-        label.disabled = value
-        checkbox.disabled = value
+        else
+        {
+            label.disabled = false;
+            cancelButton.disabled = false;
+        }
+    });
+
+    cancelButton.addEventListener('click', () => {
+        // freeze all tasks's inputs except this one
+        if (task.checked) 
+            return;
+        if (label.value == '')
+        {
+            alert("Can't cancel an empty task");
+            return;
+        }
+        const value = !label.disabled;
+        label.disabled = value;
+        checkbox.disabled = value;
 
         // swap the "enable" | "disable"
-        if (cancelButton.value =="disable"){
-            cancelButton.value ="enable"
-        } else {
-            cancelButton.value ="disable"
-        }
+        cancelButton.value = cancelButton.value == 'disable' ? 'enable' : 'disable'
     })
-    checkbox.addEventListener("change", () => {
-        // if task is checked (done) you can't change its content neither its availability (enable | disable)
-        
-        if (checkbox.checked) {
-            if (label.value == "") {
-                alert("Can't check an empty task")
-                return
-            }
-            label.disabled = true
-            cancelButton.disabled = true
+
+    deleteButton.addEventListener('click', () => {
+        if (!checkbox.disabled);
+        {
+            taskDiv.remove();
+            return;
         }
-        else {
-            label.disabled = false
-            cancelButton.disabled = false
-        }
+        alert("Can't remove a disabled task");
+    }) 
+
+    modifButton.addEventListener('click', () => {
+        // swap the modif state
+        task[3] = !task[3];
+        label.readOnly = !label.readOnly;
+        modifButton.value = modifButton.value == 'modify' ? 'save' : 'modify';
     })
 }
 
+
+let tasks = getTasks()
+console.log(tasks)
+
+tasks.forEach(task => createTask(task))
+
+// this interval is not necessary anymore
+// we need to fetch every time a task has finished to be modified
 setInterval(() => {
-    let tasks = []
-    for (element of taskContainerDiv.children) {
-        const checkbox = element.children[0]
-        const label = element.children[1]
-        const task = [label.value, checkbox.checked, checkbox.disabled]
-        if (task[0])
-            tasks.push(task)
-    }
-    console.log(tasks)
-    fetch("/api", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(tasks)
-    })
-}, 1000)
-
-
-window.addEventListener('beforeunload', function(e) {})
+    sendTasks();
+}, 5000)
 
 const createTaskButton = document.getElementById("create-task-button")
 createTaskButton.addEventListener("click", () => {
-    const label = document.createElement("input")
-    label.type = "text"
-    label.value = ""
-    const checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
-    const cancelButton = document.createElement("input")
-    cancelButton.type = "button"
-    cancelButton.value = "disable"
-    cancelButton.classList.add("cancel-button")
-    const deleteButton = document.createElement("input")
-    deleteButton.type = "button"
-    deleteButton.value = "delete"
-    deleteButton.classList.add("delete-button")
-    const taskDiv = document.createElement("div")
-    taskDiv.classList.add("task")
-    taskDiv.appendChild(checkbox)
-    taskDiv.appendChild(label)
-    taskDiv.appendChild(cancelButton)
-    taskDiv.appendChild(deleteButton)
-    taskContainerDiv.appendChild(taskDiv)
-
-    deleteButton.addEventListener("click", () => {
-        if (!checkbox.disabled) {
-            taskDiv.remove()
-            return
-        }
-        alert("Can't remove a disabled task")
-    })
-
-    cancelButton.addEventListener("click", () => {
-        // freeze all task's inputs except this one
-        if (checkbox.checked) 
-            return
-        if (label.value == "") {
-            alert("Can't cancel an empty task")
-            return
-        }
-        const value = label.disabled ? false: true
-        label.disabled = value
-        checkbox.disabled = value
-
-        // swap the "enable" | "disable"
-        if (cancelButton.value =="disable"){
-            cancelButton.value ="enable"
-        } else {
-            cancelButton.value ="disable"
-        }
-    })
-
-    checkbox.addEventListener("change", () => {
-        
-        // if task is checked (done) you can't change its content neither its availability (enable | disable)
-        if (checkbox.checked) {
-            // as the label is empty, the task can't be checked
-            if (label.value=="") {
-                checkbox.checked = false 
-                alert("Can't check an empty task")
-                return
-            }    
-            // if not empty, it can
-            label.disabled = true
-            cancelButton.disabled = true
-
-        }
-        else {
-            label.disabled = false
-            cancelButton.disabled = false
-        }
-    })
+    createTask(['', false, false])
+    tasks.push(['', false, false, false])
 })
 
+
+window.addEventListener('beforeunload', function(e) {
+    sendTasks();
+})
